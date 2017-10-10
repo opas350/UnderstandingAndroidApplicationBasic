5cfb2f1ef1af535f7ba4c61c658e881d70c24e3c
Javier Reyes <Javier.Reyes@ihg.com>
2017-10-09 19:12:53 -0500

    First Commit

--- M1/FirstExample/app/src/main/java/com/example/javierreyesvaldez/firstexample/myWorker.java
+++ M1/FirstExample/app/src/main/java/com/example/javierreyesvaldez/firstexample/myWorker.java
@@ -4,5 +4,9 @@ package com.example.javierreyesvaldez.firstexample;
  * Created by javier.reyes.valdez on 09/10/2017.
  */
 
-public class myWorker {
+public class myWorkero {
+
+    public static int doubleTheValue(int value) {
+        return value * 2;
+    }
 }
